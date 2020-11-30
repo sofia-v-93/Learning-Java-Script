39aa22f114a49d3db0c895ff3fb1866c48c483b7
sofia-v-93 <58147666+sofia-v-93@users.noreply.github.com>
2020-11-30 16:57:30 +0100

    Update and rename index.js to understanding_return.js

--- understanding_return.js
+++ understanding_return.js
@@ -4,7 +4,7 @@ let run = function(x, y) {
   x = x * 5;
   return x + y
   alert("test")
-  /*return beendet die Ausführung einer Funktion*/
+  /*return beendet die Ausführung einer Funktion der alert wird daher nicht mehr angezeigt*/
 }
 let result = run(1, 2);
 console.log(result);
