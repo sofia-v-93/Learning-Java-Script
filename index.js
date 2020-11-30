196d8adae2c0a63246f4c7401f0eba9ebd78a6eb
sofia-v-93 <58147666+sofia-v-93@users.noreply.github.com>
2020-11-30 16:56:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let run = function(x, y) {
+  x = x * 5;
+  return x + y
+  alert("test")
+  /*return beendet die Ausführung einer Funktion*/
+}
+let result = run(1, 2);
+console.log(result);
